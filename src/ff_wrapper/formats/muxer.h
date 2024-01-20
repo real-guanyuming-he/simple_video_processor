c3263ed567eaf62eecb30d0a48caf1e01929747b
Little-He-Guan <53397751+real-guanyuming-he@users.noreply.github.com>
2024-01-20 18:47:37 +0800

    Add media_file_base, muxer, and demuxer files

---
+++ src/ff_wrapper/formats/muxer.h
@@ -0,0 +1,16 @@
+#pragma once
+/*
+* Copyright (C) Guanyuming He 2024
+* This file is licensed under the GNU General Public License v3.
+*
+* This file is part of ff_wrapper.
+* ff_wrapper is free software:
+* you can redistribute it and/or modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+*
+* ff_wrapper is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with ff_wrapper.
+* If not, see <https://www.gnu.org/licenses/>.
+*/
